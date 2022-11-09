96831e72375357f4d28bedd129b6a1490e7dfc0f
Hungry_munk <abbaszadeharsam@gmail.com>
2022-11-09 18:09:20 +1100

    modal now apperas and disappers

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const addBtn = document.querySelector(".addBtn")
+const modalOveraly = document.querySelector(".modal-blocker")
+const modal = document.querySelector(".modal-form")
+const form = document.querySelector('form')
+
 let myLibrary = []
 
 function book(title,author,pages,readStatus) {
@@ -5,5 +10,23 @@ function book(title,author,pages,readStatus) {
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
-
 }
+
+addBtn.addEventListener('click',revealModal)
+modalOveraly.addEventListener('click',hideModal)
+
+function revealModal () {
+    modalOveraly.style.visibility = "visible"
+    modal.classList.add('open')
+}
+
+function hideModal() {
+    modalOveraly.style.visibility = "hidden"
+    modal.classList.remove('open')
+    resetForm()
+}
+
+function resetForm() {
+    form.reset()
+}
+
