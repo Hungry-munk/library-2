720e09e71eaf7681491e003a930f856eb14b60c7
Arsam Abbaszadeh <arsamabbaszadeh@Arsams-MacBook-Air.local>
2022-11-09 11:37:19 +1100

    added basic book HTML but not styled yet

---
+++ script.js
@@ -0,0 +1,9 @@
+let myLibrary = []
+
+function book(title,author,pages,readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+
+}
